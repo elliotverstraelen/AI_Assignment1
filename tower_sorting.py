ab95d9b27b886482d332f7da981d7b953580fb2f
dragonboyy <elliot.verstraelen@telenet.be>
2023-03-01 19:51:25 +0100

    Début de action, result & goal + test unit

--- tower_sorting.py
+++ tower_sorting.py
@@ -7,6 +7,7 @@ import time
 import sys
 from copy import deepcopy
 from search import *
+import unittest
 
 
 #################
@@ -14,14 +15,54 @@ from search import *
 #################
 class TowerSorting(Problem):
 
+    def __init__(self, initial_state):
+        self.initial = initial_state
+        self.num_towers = initial_state.number
+        self.max_tower_size = initial_state.size
+
+    # The possible actions at any given state are moving a disk from the top
+    # of one tower to another tower if and only if the target tower is either 
+    # empty or its top disk has a larger size than the disk to be moved.
     def actions(self, state):
-        pass
+        actions = []
+        # Loop through all pairs of towers
+        for i in range(self.num_towers):
+            for j in range(self.num_towers):
+                # check if the pair of towers is distrinct
+                if i != j:
+                     # check if the source tower is not empty
+                     if len(state.grid[i]) > 0:
+                        # check if the target tower is either empty 
+                        # or its top disk has a larger size
+                        # than the disk to be moved
+                        if len(state.grid[j]) == 0 or state.grid[i][-1] == state.grid[j][-1]:
+                            actions.append((i, j))
+        return actions
 
     def result(self, state, action):
-        pass
+        # Copy the current state to a new state
+        new_state = deepcopy(state)
+        # Remove the disk from the top of the source tower
+        disk = new_state.grid[action[0]].pop()
+        # Add the disk to the top of the target tower
+        new_state.grid[action[1]].append(disk)
+        # Return the new state
+        return new_state
 
     def goal_test(self, state):
-        pass
+        # Loop through all towers
+        for tower in state.grid:
+            # check if the tower is not empty
+            if len(tower) > 0:
+                # Check if all disks in the tower are of the same color
+                if len(set(tower)) != 1:
+                    return False
+        # Return True if all towers are uniform and complete
+        return True
+
+    def path_cost(self, c, state1, action, state2):
+        # 1 for each action
+        return c + 1
 
 
 ###############
@@ -46,11 +87,15 @@ class State:
             s += "\n"
         return s
 
+    # Not sure about implementation
     def __eq__(self, other):
-        pass
+        if isinstance(other, State):
+            return self.grid == other.grid
+        return False
 
+    # Not sure about implementation
     def __hash__(self):
-        pass
+        return hash(tuple(map(tuple, self.grid)))
 
 
 ######################
@@ -99,3 +144,22 @@ if __name__ == "__main__":
     print("* Path cost to goal:\t", node.depth, "moves")
     print("* #Nodes explored:\t", nb_explored)
     print("* Queue size at goal:\t",  remaining_nodes)
+
+class TestTowerSorting(unittest.TestCase):
+
+    def test_initial_state(self):
+        ts = TowerSorting(3)
+        state = ts.initial_state()
+        expected = [[3, 2, 1], [], []]
+        self.assertEqual(state.grid, expected)
+
+    def test_actions(self):
+        pass
+
+    def test_result(self):
+        pass
+
+    def test_goal_test(self):
+        pass
+
+
